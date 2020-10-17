578470da12a64a0307a558771146f9e2c44de23d
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-16 20:28:24 -0500

    Cleaned up and refactored the code

--- script/covidAPI.js
+++ script/covidAPI.js
@@ -1,8 +1,19 @@
-let COVIDDataSet=[];
-const datapoints=3;
-let period="month"; // We could let the user set this if we wanted to
+const exampleData={ 
+    city: "Chicago",
+    county:  "Cook",
+    state: "Illinois",
+    date: "December 16", 
+    pop: 5150000,
+    totalCount:  336000,
+    countPerPop:  0,
+    death: 9300,
+    deathPerPop:  0,
+    currentCount:  100000,
+    curCountPerPop:  0
+}
 
 function getData(event){
+    COVIDDataSet=[];
     let state=currentCity;
     if(!state) throw "Error, user did not submit a state";
 
@@ -24,73 +35,54 @@ function getData(event){
         }
     }    
     for(let i=0;i<datapoints;i++){
-        url="https://api.covidtracking.com/v1/states/"+"IL"+"/"+dates[i]+".json";
+        url="https://api.covidtracking.com/v1/states/"+state+"/"+dates[i]+".json";
         settings.url=url;
         $.ajax(settings).done(response=>collateCovidData(response));
     }
 
 }
-
-let _rep;
 function collateCovidData(response){
     _rep=response;
     date=response.date.toString();
-    date=moment(date).format("LL");
+    humanDateFormat=moment(date).format("LL");
 
-    let newData={
- 
+    let newData={ 
         // city: "Chicago",
         // county:  "Cook",
         state: response.state,
-        date: date, 
+        humanDateFormat: humanDateFormat, 
+        date: date,
         // pop: 5150000,
         totalCount:  response.positive,
 //         countPerPop:  0,
         death: response.death,
         // deathPerPop:  0,
         // currentCount:  0,
-        // curCountPerPop:  0
-    
+        // curCountPerPop:  0    
     }
     COVIDDataSet.push(newData);
     sortData();
-
+    storeData();
 }
 
 function sortData(){
     COVIDDataSet=COVIDDataSet.sort(function(a,b){
+        // Ideally this date info should be stored in ISO format
         if(moment(a.date).format("X")>moment(b.date).format("X")) return 1;
         else return -1;
     });
-
 }
 
 function storeData(){
+    console.log(COVIDDataSet);
     allData=[];
     population=[];
     dateLabel=[];
-    for(const value of COVIDDataSet)
-    const newConstData=new Data(COVIDDataSet[value]);
+    for(let i=0; i< COVIDDataSet.length; i++){
+    const newConstData=new Data(COVIDDataSet[i]);
     allData.push(newConstData);
-    population.push(COVIDDataSet[value].totalCount);
-    dateLabel.push(COVIDDataSet[value].date);
+    population.push(COVIDDataSet[i].totalCount);
+    dateLabel.push(COVIDDataSet[i].humanDateFormat);}
     displayData();
     makeNewChart();
-
 }
-
-
-
-// function displayCovidData(){
-//     var COVIDBox=$("<div>").addClass("COVID-box");
-//     var bodyText=`<p>On ${date},</p><p> ${dead} people were </p><p>confirmed to have died of COVID.</p>`;
-//     var modalHTML=`<div class="COVID-modal"><div class="COVID-modal-header"><span class="COVID-modal-exit">X</span><div class="COVID-modal-body">${bodyText}</div></div></div>`;
-//     COVIDBox.html(modalHTML);
-//     $("body").append(COVIDBox);
-//     $(".COVID-box").on("click",function(){$(".COVID-box").remove();})
-// }
-
-// const newConstData=new Data(newData);
-// COVIDDataSet.push(newConstData);
-// population.push(newData.death);
-// dateLabel.push(newData.date);
